0b5e374207b4de859acb9979eeec24925d985c7b
Matias <matias.schmitz@gmail.com>
2022-10-05 18:41:10 -0300

    primer intento de join para getAll products

--- app/models/product.model.php
+++ app/models/product.model.php
@@ -14,7 +14,7 @@ class ProductModel{
     }
 
     function getAll(){
-        $query = $this->db->prepare('SELECT * from products');
+        $query = $this->db->prepare('SELECT p.name, p.description, p.price, c.name from products p INNER JOIN categories c ON p.id_category_fk = c.id');
         $query->execute();
 
         $products = $query->fetchAll(PDO::FETCH_OBJ);
--- db/lify.sql
+++ db/lify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-10-2022 a las 18:08:01
+-- Tiempo de generación: 05-10-2022 a las 23:40:01
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -29,9 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
-  `description` int(11) NOT NULL
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES
+(6, 'Microgreen', 'Son plantulas jovenes, con los mismos sabores y nutrientes que sus versiones adultas pero en una mayor concentración. Ideales para decorar tus comidas y darles un toque \"gourmet\".'),
+(7, 'Aromatica', 'Su nombre lo dice todo, más allá de ser un elemento decorativo son un gran aporte al aroma y sabor de tu plato.'),
+(8, 'Flor comestible', 'Si bien algunas tienen sabor, el fuerte indebatible es la estética que aportan a los platos o postres.');
 
 -- --------------------------------------------------------
 
@@ -46,6 +55,13 @@ CREATE TABLE `products` (
   `price` float NOT NULL,
   `id_category_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `id_category_fk`) VALUES
+(2, 'Rabanito', 'Picante', 5.82, 6);
 
 --
 -- Índices para tablas volcadas
@@ -72,13 +88,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT de la tabla `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
