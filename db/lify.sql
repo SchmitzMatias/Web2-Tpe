4b04e1e772229e9067ad8dbe278759f103b6332b
Matias <matias.schmitz@gmail.com>
2022-10-17 22:22:41 -0300

    actualizado datos base

--- db/lify.sql
+++ db/lify.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-10-2022 a las 23:40:01
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 18-10-2022 a las 03:21:55
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,7 +38,7 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `description`) VALUES
-(6, 'Microgreen', 'Son plantulas jovenes, con los mismos sabores y nutrientes que sus versiones adultas pero en una mayor concentración. Ideales para decorar tus comidas y darles un toque \"gourmet\".'),
+(6, 'Microgreen', 'Son plántulas jóvenes, con los mismos sabores y nutrientes que sus versiones adultas pero en una mayor concentración.'),
 (7, 'Aromatica', 'Su nombre lo dice todo, más allá de ser un elemento decorativo son un gran aporte al aroma y sabor de tu plato.'),
 (8, 'Flor comestible', 'Si bien algunas tienen sabor, el fuerte indebatible es la estética que aportan a los platos o postres.');
 
@@ -61,7 +61,32 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `id_category_fk`) VALUES
-(2, 'Rabanito', 'Picante', 5.82, 6);
+(2, 'Rabanito', 'De sabor fresco y picante, es ideal para complementar tus ensaladas o realzar tus platos con pescado', 5.82, 6),
+(9, 'Calendula', 'Color naranja brillante y notas cítricas', 200, 8),
+(10, 'Nira', 'Tradicional de la cocina asiática, tiene un suave sabor entre ajo y verdeo', 50, 7),
+(14, 'Remolacha', 'Se caracterizan por su llamativo tallo rojo con hojas verdes.', 10.82, 6),
+(15, 'Rucula', 'Mismo sabor que su contraparte adulta, pero potenciada.', 8.08, 6),
+(16, 'Tomillo limon', 'Similar al tomillo clásico, pero con agradable aroma cítrico', 50, 7),
+(17, 'Hibiscus', 'Popular en oriente, esta flor se caracteriza por su agradable textura y sabor.', 200, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+(1, 'admin@admin.com', '$2a$12$ae0i4v449xY.cv21V5FNdeZ8EBRphFcNjqtLW8WzzdfSgrAGceov6');
 
 --
 -- Índices para tablas volcadas
@@ -81,6 +106,12 @@ ALTER TABLE `products`
   ADD KEY `fk_category` (`id_category_fk`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -88,13 +119,19 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT de la tabla `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
